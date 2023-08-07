3af0d882d54cfef42d140757ba50ed3674b45a64
oleksandrmorgun <127879303+oleksandrmorgun@users.noreply.github.com>
2023-08-07 13:20:26 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+stopBtnEl.setAttribute('disabled', 'true');
+
+function onColorSwitcherStart() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  stopBtnEl.removeAttribute('disabled');
+  startBtnEl.setAttribute('disabled', 'true');
+}
+
+function onColorSwitcherStartStop() {
+  clearInterval(timerId);
+  startBtnEl.removeAttribute('disabled');
+  stopBtnEl.setAttribute('disabled', '');
+}
+startBtnEl.addEventListener('click', onColorSwitcherStart);
+stopBtnEl.addEventListener('click', onColorSwitcherStartStop);
